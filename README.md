f36e66270ed6971caf78a6ed83086afc3d9855a7
qawizard <qawizard@yandex-team.ru>
2017-07-31 19:24:13 +0300

    Added first example of CatBoost ussage

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CatBoostExamples
+
+This repository contains simple examples
+of ussage of CatBoost library.
+
+This library allows to create AI systems which can
+simulate the intelegence, for instance, predict something based on
+the test data.
---
+++ predict_number/predict.py
@@ -0,0 +1,40 @@
+# This code shows how we can use CatBoot lirary to classify
+# some objects and identify the object
+#
+# How to run:
+#     python predict.py
+#
+
+import numpy as np
+from catboost import CatBoostClassifier
+
+
+# Train data which will be used to train our NeuronNet.
+# Here [0, 0, 0, 0, 1] means '1', [0, 0, 1, 0, 1] means 5 and etc.
+# so we just define the correct data to teach our AI:
+train_data = [[0, 0, 0, 0, 1], [0, 0, 0, 1, 0], [0, 0, 0, 1, 1],
+              [0, 0, 1, 0, 0], [0, 0, 1, 0, 1], [0, 0, 1, 1, 0]]
+train_label = ['1', '2', '3', '4', '5', '6']
+
+# Some test data which we want to classify somehow. Let's imagine
+# that we don't know what [0, 0, 0, 0, 1] means. And let's ask AI
+# to recognize this object for us:
+test_data = [[1, 0, 1, 0, 1]]
+
+
+# Specify the training parameters:
+model = CatBoostClassifier(iterations=1000, thread_count=16,
+                           loss_function='MultiClass',
+                           verbose=True)
+
+
+# Train the model using prepared right data:
+model.fit(train_data, train_label, verbose=True)
+
+
+# Make the prediction using the resulting model
+preds_class = model.predict(test_data)
+
+# Print the prediction:
+print('This object is {0}'.format(preds_class[0][0]))
+
